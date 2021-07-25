de08c8f23daeffc5563a23819930424f3d257d8a
root <root@DESKTOP-28JH53O.localdomain>
2021-07-25 09:18:48 -0500

    addPullerOk

--- dags/puller.py
+++ dags/puller.py
@@ -694,6 +694,7 @@ def puller_idirect():
     send_qq_mongo= send_queque_kafka(secondary_vs_mongo,'updatemongo','not_exist_mongo_secondary') 
     send_qq_mongo_timep= send_queque_kafka(secondary_vs_mongo,'updatemongotimep','exist_mongo_secondary') 
     save_in_redis_end = save_in_redis(config,platform_data)
+    [secondary_vs_mysql,secondary_vs_mongo] >> save_in_redis_end >> finish
 
 
     # [END main_flow]
