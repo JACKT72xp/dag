7adb18fa39b118c6826a877f5b130d101d905e6f
root <root@DESKTOP-28JH53O.localdomain>
2021-07-26 13:32:39 -0500

    editarg

--- dags/puller_arg.py
+++ dags/puller_arg.py
@@ -625,11 +625,9 @@ def puller_idirect_argentina():
 
 
     # [START main_flow]
-    rs = start()
-    response_verify = verify(rs)
-    if response_verify is None:
-        finish(response_verify)
-        return 'ok'
+
+
+
     config = [
       {
         "route_trunk": "data",
@@ -680,6 +678,16 @@ def puller_idirect_argentina():
     data_mdb = coltn_mdb.find({'platform':38})
 
 
+
+
+    rs = start()
+    response_verify = verify(rs)
+    if response_verify is None:
+        finish(response_verify)
+        return 'ok'
+
+
+
     key_process = str(config["platform_id"])+"-"+str(config["platform_name"])
     platform_data = extract_platform(config,response_verify)
     old_data = extract_old(key_process,config,response_verify)
@@ -691,13 +699,13 @@ def puller_idirect_argentina():
     # send_qq_delete_mongo= send_queque_kafka(comp,'deletemongo','only_old') 
     
     mysql_data = extract_mysql(engine,config,response_verify)
+    mongo_data = extract_mongo(data_mdb,key_process_mongo,config,response_verify)
     primary_vs_mysql = comparate_primary_mysql(mysql_data,comp)
     send_qq_insert_vsmysql= send_queque_kafka(primary_vs_mysql,'insertmysqlarg','not_exist_mysql') 
     secondary_vs_mysql = comparate_secondary_mysql(mysql_data,primary_vs_mysql)
     send_qq= send_queque_kafka(secondary_vs_mysql,'updatemysqlarg','not_exist_mysql_secondary') 
-
     key_process_mongo = key_process
-    mongo_data = extract_mongo(data_mdb,key_process_mongo,config,response_verify)
+
     primary_vs_mongo = comparate_primary_mongo(mongo_data,comp)
     send_qq_insert_vsmongo= send_queque_kafka(primary_vs_mongo,'insertmongoarg','not_exist_mongo') 
   
