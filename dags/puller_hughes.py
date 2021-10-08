011b5115f6ed089dbd8a6a5ffc00a606fd95b166
root <root@DESKTOP-28JH53O.localdomain>
2021-10-08 14:51:55 -0500

    ok

--- dags/puller_hughes.py
+++ dags/puller_hughes.py
@@ -860,7 +860,8 @@ def puller_hughes():
     save_in_redis_end = save_in_redis_data_old(config,platform_data,key_process)
 
     end = finish([{"status":True}])
-    rs >> [old_data , platform_data] >> [primary_vs_mysql_equals >> secondary_vs_mysql_equals >>  save_in_redis_result_equals >> send_key_redis_to_api_equals,primary_vs_mysql_only_platform >> secondary_vs_mysql_only_platform >> save_in_redis_result_only_platform >> send_key_redis_to_api_only_platform  ] >> save_in_redis_end >> end
+    rs >> [old_data , platform_data]
+    [primary_vs_mysql_equals >> secondary_vs_mysql_equals >>  save_in_redis_result_equals >> send_key_redis_to_api_equals,primary_vs_mysql_only_platform >> secondary_vs_mysql_only_platform >> save_in_redis_result_only_platform >> send_key_redis_to_api_only_platform  ] >> save_in_redis_end >> end
 
     # [END main_flow]
 
