c8f16415ce9f5cfc250e76a54859888f8321ef04
root <root@DESKTOP-28JH53O.localdomain>
2021-10-12 21:10:05 -0500

    sorting

--- dags/puller_hughes.py
+++ dags/puller_hughes.py
@@ -38,6 +38,7 @@ uri = "mongodb://bifrostProdUser:Maniac321.@cluster0-shard-00-00.bvdlk.mongodb.n
 conection = MongoClient(uri)
 db_ = conection["bifrost"]
 
+coltn_mdb = db_['hughes_test']
 
 
 # import confluent_kafka
@@ -128,7 +129,6 @@ def puller_hughes():
       }
     ]
     config = config[0]
-    coltn_mdb = db_[config["mongo_collection"]]
     data_mdb = coltn_mdb.find({})
 
     def generateConcatKeySecondary(df,cols):
@@ -281,9 +281,9 @@ def puller_hughes():
 
     @task()
     def save_in_history_mongo(config):
-        uri = "mongodb://bifrostProdUser:Maniac321.@cluster0-shard-00-00.bvdlk.mongodb.net:27017,cluster0-shard-00-01.bvdlk.mongodb.net:27017,cluster0-shard-00-02.bvdlk.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-nn38a4-shard-0&authSource=admin&retryWrites=true&w=majority"
-        conection = MongoClient(uri)
-        db_ = conection["bifrost"]
+        # uri = "mongodb://bifrostProdUser:Maniac321.@cluster0-shard-00-00.bvdlk.mongodb.net:27017,cluster0-shard-00-01.bvdlk.mongodb.net:27017,cluster0-shard-00-02.bvdlk.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-nn38a4-shard-0&authSource=admin&retryWrites=true&w=majority"
+        # conection = MongoClient(uri)
+        # db_ = conection["bifrost"]
         coltn_mdb = db_['puller_history']
         time_send = datetime.now()
         formatted_date = time_send.strftime('%Y-%m-%d %H:%M:%S')
