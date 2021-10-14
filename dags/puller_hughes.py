5d13543ddaeb7f043faea53d23b06ecf85ebec74
root <root@DESKTOP-28JH53O.localdomain>
2021-10-14 14:54:55 -0500

    selectRowsInQueryMongo

--- dags/puller_hughes.py
+++ dags/puller_hughes.py
@@ -881,7 +881,7 @@ def puller_hughes():
     rs = start()
     valid_puller_runing = valid_exist_puller_runing()
     print(valid_puller_runing)
-    if valid_puller_runing.state is None:
+    if valid_puller_runing is None:
         end = finish([{"status":True}])
         print(valid_puller_runing.items)
     else:
