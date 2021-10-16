6c5e8c0b60eacca8e0dd9837d181c85a3ee05b36
root <root@DESKTOP-28JH53O.localdomain>
2021-10-16 03:22:13 -0500

    addMongo

--- dags/puller_hughes.py
+++ dags/puller_hughes.py
@@ -150,7 +150,7 @@ def puller_hughes():
                     df_stnd_key[col] =  df_stnd_key[col].map(lambda eve: eve.replace(".0",""))
                     df[col]=df_stnd_key[col]
                 if(col=='mongo_esn'):
-                    df_stnd_key[col] =  df_stnd_key[col].map(lambda eve: eve.replace(".0"," "))
+                    df_stnd_key[col] =  df_stnd_key[col].map(lambda eve: eve.replace(".0",""))
                     df[col]=df_stnd_key[col]
             df_stnd_key[col] =  df_stnd_key[col].map(lambda eve: eve.replace("0.0000000000000000"," "))
             df_stnd_key['concat_key_generate_secondary'] = df_stnd_key[cols].agg('-'.join, axis=1)
