437ce14ec08d3304fb17f84b1f16129e2c7bd0f8
root <root@DESKTOP-28JH53O.localdomain>
2021-10-21 12:10:25 -0500

    removesavehistoryMysql

--- dags/puller_hughes.py
+++ dags/puller_hughes.py
@@ -626,11 +626,12 @@ def puller_hughes():
             
             if  config['route_trunk'] == "":
                 response =  pd.DataFrame(response) 
+                xaa=response[response['deviceID']=='1600032794']
+                print(xaa[['latitude','longitude']],'aaa')
                 response = response[response.columns].add_prefix('platform_')
                 response = generateConcatKey(response,['platform_'+config['primary_join_cols']['platform']])
                 response = generateConcatKeySecondary(response,config['secondary_join_cols']['platform'])
-                xaa=response[response['platform_deviceID']=='1600032794']
-                print(xaa[['platform_latitude','platform_longitude']],'aaa')
+
                 response = response.to_json(orient='records')
                 response = json.loads(response)
                 return response
