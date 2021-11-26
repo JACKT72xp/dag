5f49d946dc5bc226d22b089ea7bcca97b8cc8b90
unknown <julio_hlr@hotmail.com>
2021-11-26 17:02:05 -0500

    scheduler

--- dags/puller_newtec.py
+++ dags/puller_newtec.py
@@ -80,7 +80,7 @@ default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "retry_delay": timedelta(seconds=15),
-    "start_date": datetime(2021, 11, 26, 17, 0),
+    "start_date": datetime(2021, 11, 26, 22, 0),
     # 'email': ['tech.team@industrydive.com'],
     # 'email_on_failure': True,
     # 'email_on_retry': True,
