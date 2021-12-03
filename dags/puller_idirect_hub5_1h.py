49214a598b62e32d9277cb9e8780d007b5deb05f
unknown <julio_hlr@hotmail.com>
2021-12-03 12:40:40 -0500

    ok

--- dags/puller_idirect_hub5_1h.py
+++ dags/puller_idirect_hub5_1h.py
@@ -462,10 +462,10 @@ def puller_idirect_hub5_1h():
             return []
             
         data_insert_send = pd.DataFrame(data)
-        # print(data_insert_send,' data_insert_senddata_insert_senddata_insert_senddata_insert_send')
-        # print(keys,' keyskeyskeyskeyskeys')
-        print(json.loads(template_orders)[0],' template_orderstemplate_orders')
-        print(json.loads(template_orders)[0]['input'],' inputinputinputinput')
+        json_template= json.loads(template_orders)[0]
+        input_template = json_template['input']
+        print(json_template,' json_templatejson_templatejson_template')
+        print(input_template,' input_templateinput_templateinput_templateinput_template')
         # data_insert_send = data_insert_send[['platform_esn','platform_deviceID','platform_latitude','platform_longitude','platform_terminalStatus','platform_esn']]
         # data_insert_send.rename(columns={"platform_deviceID": "siteId"}, inplace = True)
         # data_insert_send.rename(columns={"platform_latitude": "latitud"}, inplace = True)
