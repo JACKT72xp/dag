ef04a643f250fb593786cdf8f1d5e2942371d72e
unknown <julio_hlr@hotmail.com>
2021-12-03 14:32:06 -0500

    editHughes

--- dags/puller_hughes.py
+++ dags/puller_hughes.py
@@ -1032,8 +1032,10 @@ def puller_hughes():
         else:
             
             data_mysql_not_exist_s = df_mysql[df_mysql['concat_key_generate'].isin(list(not_exist_mysql_s_com['concat_key_generate']))]
-            print(not_exist_mysql_s_com['concat_key_generate'],'  -asdasdasdasdasd')
             print(df_mysql[df_mysql['concat_key_generate']=='ORBITHB600301'],'df_mysqldf_mysqldf_mysqldf_mysqldf_mysql')
+            print(data_mysql_not_exist_s[data_mysql_not_exist_s['concat_key_generate']=='ORBITHB600301'],'data_mysql_not_exist_sdata_mysql_not_exist_sdata_mysql_not_exist_sdata_mysql_not_exist_s')
+            print(not_exist_mysql_s_com[not_exist_mysql_s_com['concat_key_generate']=='ORBITHB600301'],'data_mysql_not_exist_sdata_mysql_not_exist_sdata_mysql_not_exist_sdata_mysql_not_exist_s')
+            
             data_mysql_not_exist_s = pd.merge(not_exist_mysql_s_com, data_mysql_not_exist_s, on="concat_key_generate")
             # print(data_mysql_not_exist_s[['concat_key_generate_secondary_x','concat_key_generate_secondary_y','platform_deviceID']],'hereeeeeee')
             data_mysql_not_exist_s = json.loads(data_mysql_not_exist_s.to_json(orient="records"))
