09dc41ded81c64679884ac574a1e9fae3d147a35
unknown <julio_hlr@hotmail.com>
2021-12-03 14:45:48 -0500

    editHughes

--- dags/puller_hughes.py
+++ dags/puller_hughes.py
@@ -1031,9 +1031,8 @@ def puller_hughes():
             data_mysql_not_exist_s = []
         else:
             
+            print(not_exist_mysql_s_com[not_exist_mysql_s_com['platform_deviceID']=='ORBITHB600301'],'not_exist_mysql_s_comnot_exist_mysql_s_comnot_exist_mysql_s_comnot_exist_mysql_s_com')
             data_mysql_not_exist_s = df_mysql[df_mysql['concat_key_generate'].isin(list(not_exist_mysql_s_com['concat_key_generate']))]
-            print(df_mysql[df_mysql['platform_deviceID']=='ORBITHB600301'],'df_mysqldf_mysqldf_mysqldf_mysqldf_mysql')
-            # print(data_mysql_not_exist_s[data_mysql_not_exist_s['platform_deviceID']=='ORBITHB600301'],'data_mysql_not_exist_sdata_mysql_not_exist_sdata_mysql_not_exist_sdata_mysql_not_exist_s')
             # print(not_exist_mysql_s_com[not_exist_mysql_s_com['platform_deviceID']=='ORBITHB600301'],'data_mysql_not_exist_sdata_mysql_not_exist_sdata_mysql_not_exist_sdata_mysql_not_exist_s')
             
             data_mysql_not_exist_s = pd.merge(not_exist_mysql_s_com, data_mysql_not_exist_s, on="concat_key_generate")
