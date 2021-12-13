439fb74ae652d2c3339c843b70df07879c60fece
unknown <julio_hlr@hotmail.com>
2021-12-13 13:29:15 -0500

    removeOldIdirectArgPuller

--- dags/puller_hughes.py
+++ dags/puller_hughes.py
@@ -64,13 +64,14 @@ r = redis.Redis(host= '192.168.29.20',    port= '6379',    password="bCL3IIuAwv"
 default_args = {
     'owner': 'airflow',
     'retry_delay': timedelta(seconds=20),
+    "start_date": datetime(2021, 12, 14, 0, 0),
     # "start_date": datetime(2021, 12, 12, 20, 0),
     # 'email': ['tech.team@industrydive.com'],
     # 'email_on_failure': True,
     # 'email_on_retry': True,
     # 'max_active_runs':2,
     'depends_on_past': True,
-    # 'max_active_tis_per_dag':1,
+    'max_active_tis_per_dag':2,
     # 'concurrency':5,
     # 'schedule_interval':timedelta(minutes=10),
     'retries': 2,
@@ -82,8 +83,8 @@ default_args = {
 time_send_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 # [START instantiate_dag]
-@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['hughes'])
-# @dag(default_args=default_args, schedule_interval='*/10 * * * *',  tags=['hughes'])
+# @dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['hughes'])
+@dag(default_args=default_args, schedule_interval='*/10 * * * *',  tags=['hughes'])
 def puller_hughes():
     
     # sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
