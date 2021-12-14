0096c28a15f1d84b22c7f16696441aa9a5a66487
JACKT72xp <48709035+JACKT72xp@users.noreply.github.com>
2021-12-14 16:23:31 +0100

    Update puller_hughes.py

--- dags/puller_hughes.py
+++ dags/puller_hughes.py
@@ -65,7 +65,7 @@ default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "retry_delay": timedelta(seconds=15),
-    # "start_date": datetime(2021, 10, 19, 1, 0),
+    "start_date": datetime(2021, 12, 15, 1, 0),
     # 'email': ['tech.team@industrydive.com'],
     # 'email_on_failure': True,
     # 'email_on_retry': True,
